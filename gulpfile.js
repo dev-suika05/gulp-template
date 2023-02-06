507ddf855e1c0c1bddecd79338e37ff7185badd4
suika05 <dev.suika05@gmail.com>
2023-02-06 21:57:55 +0900

    babel追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp = require('gulp');//gulp本体
+const gulp = require('gulp');
 
-//scss
-const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
-const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
-const notify = require("gulp-notify"); // エラー発生時のアラート出力
-const browserSync = require("browser-sync"); //ブラウザリロード
+const sass = require('gulp-dart-sass');
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const browserSync = require("browser-sync");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 
 
-// 入出力するフォルダを指定
 const srcBase = './src';
 const assetsBase = './src/assets';
 const distBase = './dist';
@@ -19,44 +19,55 @@ const srcPath = {
 };
 
 const distPath = {
+  'html': distBase + '/',
   'css': distBase + '/css/',
-  'html': distBase + '/'
+  'js': distBase + '/js/',
+  'img': distBase + '/img/',
 };
 
-/**
- * sass
- *
- */
 const cssSass = () => {
   return gulp.src(srcPath.scss, {
     sourcemaps: true
   })
     .pipe(
-      //エラーが出ても処理を止めない
       plumber({
         errorHandler: notify.onError('Error:<%= error.message %>')
       }))
-    .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
-    .pipe(gulp.dest(distPath.css, { sourcemaps: './' })) //コンパイル先
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest(distPath.css, { sourcemaps: './' }))
     .pipe(browserSync.stream())
-    .pipe(notify({
-      message: 'Sassをコンパイルしました！',
-      onLast: true
-    }))
 }
 
+const jsBabel = () => {
+  return src(srcPath.js)
+    .pipe(
+      plumber(
+        {
+          errorHandler: notify.onError('Error: <%= error.message %>')
+        }
+      )
+    )
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(dest(destPath.js))
+    .pipe(uglify())
+    .pipe(
+      rename(
+        { extname: '.min.js' }
+      )
+    )
+    .pipe(dest(destPath.js))
+}
 
-/**
- * html
- */
+exports.default = series(cssSass, jsBabel);
+
+
 const html = () => {
   return gulp.src(srcPath.html)
     .pipe(gulp.dest(distPath.html))
 }
 
-/**
- * ローカルサーバー立ち上げ
- */
 const browserSyncFunc = () => {
   browserSync.init(browserSyncOption);
 }
@@ -65,29 +76,16 @@ const browserSyncOption = {
   server: distBase
 }
 
-/**
- * リロード
- */
 const browserSyncReload = (done) => {
   browserSync.reload();
   done();
 }
 
-/**
- *
- * ファイル監視 ファイルの変更を検知したら、browserSyncReloadでreloadメソッドを呼び出す
- * series 順番に実行
- * watch('監視するファイル',処理)
- */
 const watchFiles = () => {
   gulp.watch(srcPath.scss, gulp.series(cssSass))
   gulp.watch(srcPath.html, gulp.series(html, browserSyncReload))
 }
 
-/**
- * seriesは「順番」に実行
- * parallelは並列で実行
- */
 exports.default = gulp.series(
   gulp.parallel(html, cssSass),
   gulp.parallel(watchFiles, browserSyncFunc)
